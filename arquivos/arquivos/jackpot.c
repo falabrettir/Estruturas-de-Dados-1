5cbd546b442f2d3b994866235b1de68d059011a0
Falabretti <141967784+falabrettir@users.noreply.github.com>
2024-03-11 15:23:20 -0300

    Online

---
+++ arquivos/arquivos/jackpot.c
@@ -0,0 +1,13 @@
+#include "queue.h"
+#include <time.h>
+
+void jackpot (int n, int r) {
+  /*Terminar*/
+}
+
+int main () {
+  srand(time(NULL));	
+  int n = 3;  /*número de carretéis*/ 
+  int r = 10; /*sequência de números em cada carretel*/ 
+  jackpot (n, r);
+}
