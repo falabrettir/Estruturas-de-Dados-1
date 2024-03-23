56c4922921a9ad2520d6bb291af1ec188a7b34c3
Fala <fala@Rodrigo-PC>
2024-03-23 12:52:29 -0300

    Stack

--- stack/arquivos/elimina-par.c
+++ stack/arquivos/elimina-par.c
@@ -3,12 +3,24 @@
 int main () {
   int tam = 6;
   Stack *s = create (tam);
+  Stack *impar = create(tam);
   srand(time(NULL));
-  int i;
+  int i, e;
   for (i = 0; i < tam; i++) {
     push (s, rand()%10);
   }
   print (s);
-  /*TERMINAR*/
+  while(!empty(s)){
+    e = pop(s);
+    if(e&1)
+      push(impar, e);	      
+  }
+  i = 0;
+  while(!empty(impar)){
+    push(s, pop(impar));
+  }
+  print(s);
+
   destroy (s);
+  destroy (impar);
 }
